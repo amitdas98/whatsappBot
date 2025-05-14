ca58f3e78bb4bccb352277d913fe2b452e830dd1
Amit das <mailamitad98@gmail.com>
2025-05-14 23:56:27 +0600

    test

--- controller/waSenderAPI.js
+++ controller/waSenderAPI.js
@@ -9,7 +9,12 @@ const sendMessage = async (to, text) => {
     }, {
         headers: {
             Authorization: `Bearer ${WASENDER_API_KEY}`,
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Origin": "https://wasenderapi.com",
+            "Referer": "https://wasenderapi.com/"
         }
     });
     return response.data;
