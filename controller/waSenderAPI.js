66c8b089c9a301a342ba842456cea030afdc7216
Amit das <mailamitad98@gmail.com>
2025-05-15 01:08:04 +0600

    test

--- controller/waSenderAPI.js
+++ controller/waSenderAPI.js
@@ -8,9 +8,14 @@ const sendMessage = async (to, text) => {
 	const response = await fetch("https://www.wasenderapi.com/api/send-message", {
 		method: "POST",
 		headers: {
-			Authorization: `Bearer ${WASENDER_API_KEY}`,
 			"Content-Type": "application/json",
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36",
+			Accept: "*/*",
+			"Accept-Language": "*",
+			"Sec-Fetch-Mode": "cors",
+			"Accept-Encoding": "br, gzip, deflate",
+			Host: "wasenderapi.com",
+			"User-Agent":
+				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36",
 		},
 		body: JSON.stringify({
 			to,
